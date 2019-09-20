c62a088e152dea9bd9bbf32520846c42d64766db
ithilwing <tatkirkar@gmail.com>
2019-09-20 19:14:40 +0300

    Test

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     Vector my_vector;
     Vector new_vector(5,6,7,8);
     Vector copied_vector(my_vector);
-    my_vector.setValue(1, 2, 3, 4);
+    my_vector.setValue(7, 2, 3, 4);
     my_vector.printCoordinates();
     new_vector.printCoordinates();
     copied_vector.printCoordinates();
